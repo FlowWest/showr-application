bab96cd7a601ca0a366f1a1af36eaaa4b21c09a9
ergz <erodriguez@flowwest.com>
2018-04-11 11:32:13 -0700

    refactor atu calculations to be more human readable

--- atu.R
+++ atu.R
@@ -5,36 +5,42 @@ get_gage_location <- function(redd_location) {
 }
 
 
-# given a date of observation and the location of it, we can estiamte emeergence
-estimate_emergence <- function(.date, redd_location) {
+# given a date of observation and the location of it, we can estiamte emergence 
+# using Accumulated Temperature Units
+# the logic is simple, use the date of spawn as the start of accumulation
+# use the the reach of spawn to select appropriate cdec location.
+# summarize cdec temp data to daily mean, and accumulate until the value of 1800 is
+# obtained.
+estimate_emergence <- function(spawn_date, redd_location) {
   # get the appropriate cdec/cvtemp location 
   gage <- get_gage_location(redd_location)
+  spawn_data <- as_date(spawn_date)
   
-  daily_temps <- 
-    temp_data %>% 
+  # summarize hourly data to a daily mean
+  atu <- 
+    temp_data %>%
     filter(location_id == gage) %>% 
     group_by(location_id, date = as_date(datetime)) %>% 
-    summarise(daily_mean = mean(parameter_value, na.rm = TRUE))
+    summarise(daily_mean = mean(parameter_value, na.rm = TRUE)) %>%
+    ungroup() %>%
+    arrange(date) %>% 
+    filter(date >= spawn_date) %>% 
+    mutate(atu = cumsum(daily_mean))
   
-  atu_df <- daily_temps %>% 
-    filter(location_id == gage, date >= .date) %>% 
-    mutate(atu = cumsum(daily_mean)) 
+    
+  emergence <- atu %>% filter(atu >= 1724)
   
-  emergence <- atu_df %>% filter(atu >= 1800)
-  
+  # if the 'emergence' dataframe is empty then we have not reached
+  # the emergence value, and must proceed using modeled temp data.
   if (nrow(emergence) == 0) {
-    current_atu <- atu_df %>% tail(1) %>% pull(atu)
+    current_atu <- atu %>% tail(1) %>% pull(atu) # store latest accumulated value
     
-    if (length(current_atu) == 0) current_atu <- 0
+    if (length(current_atu) == 0) current_atu <- 0 
     
-    thresh <- 1800 - current_atu
-    # to debug
-    #cat("Model needs to get to: ",thresh, "\n\n")
+    thresh <- 1724 - current_atu # new thresh will be old thresh minus current atu
     
-    # NOTE: the added >=.date here is for projecting of yet unseen redds
-    # to maybe provide hypothetical redd mapping
     model_estimated_emergence <- 
-      model_temps %>% filter(location_id == gage, datetime >= .date) %>% 
+      model_temps %>% filter(location_id == gage, datetime >= spawn_date) %>% 
       mutate(atu = cumsum(temp_50)) %>%
       filter(atu >= thresh) %>% 
       arrange(datetime) %>% 
