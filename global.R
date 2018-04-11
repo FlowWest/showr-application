d7781a8b39fa4ea70cbdfdef966b2907dbf2e3a1
ergz <erodriguez@flowwest.com>
2018-04-11 11:01:14 -0700

    remove old data frame no longer needed

--- global.R
+++ global.R
@@ -30,7 +30,7 @@ source("modules/about-page.R")
 load("data/general-objects.RData")
 
 
-diversion_data <- read_csv("data/srsc_diversion_data.csv") %>% 
+diversion_data <- read_csv("data/flows/srsc_diversion_data.csv") %>% 
   mutate(draft_date = mdy(draft_date))
 
 
@@ -88,10 +88,7 @@ redd_data <-
                     counts = col_double()
                   )) %>% filter(race == "Winter")
 
-
-redd_locations <- distinct(redd_data, location) %>% pull(location)
-
-carcass_data <- read_rds("data/carcass_static_data.rds")
+carcass_data <- read_rds("data/chinook/carcass_static_data.rds")
 
 # shape files for redd map 
 redd_reach <- readOGR("data/redd_reaches/redd_reach.shp", stringsAsFactors = FALSE)
@@ -115,39 +112,17 @@ carcass_data <-
 tcd_configs_data <- read_rds("data/tcd_configurations/tcd_configs_through_2017-08-24.rds")
 
 # temp locations metadata 
-cdec_temperature_locations <- read_rds("data/cdec_temperature_locations.rds")
+cdec_temperature_locations <- read_rds("data/temperatures/cdec_temperature_locations.rds")
 
 # redd location to cdec location lookup table 
-redd_to_cdec_location <- read_rds("data/redd_location_to_cdec_location.rds")
+redd_to_cdec_location <- read_rds("data/chinook/redd_location_to_cdec_location.rds")
 
 model_temps <- read_csv("https://s3-us-west-2.amazonaws.com/svproducers-data/cvtemp_data/2017-08-25_10%3A00%3A15_model_output.csv")
 model_temps$datetime <- as_date(model_temps$datetime)
 
 # winter run presence 
-wr_presence_data <- read_csv("data/wr_chinook_presence.csv")
+wr_presence_data <- read_csv("data/chinook/wr_chinook_presence.csv")
 
 pretty_num <- function(num, places = 2) {
   format(round(num, places), big.mark = ',', drop = FALSE)
 }
-
-#TODO(emanuel) this is a hack at the moment, need to fix the source soon! -ergz
-# redd to cdec
-redd_to_cdec_location$location <- redd_to_cdec_location$location %>% 
-  str_replace_all("Br ", "Bridge ") %>%
-  str_replace_all("Br$", "Bridge") %>% 
-  str_replace_all("Rd", "Road")
-
-
-
-# textInputRow<-function (inputId, label, value = "") 
-# {
-#   div(style="display:inline-block",
-#       tags$label(label, `for` = inputId), 
-#       tags$input(id = inputId, type = "text", value = value,class="input-small"))
-# }
-# 
-# cold_water_pool_2018 <- 
-#   gs_url("https://docs.google.com/spreadsheets/d/1fkl4RTjOADGNjgUD56HLKMdC0xd1olaw91PFbKiNY3s/edit?usp=sharing") %>% 
-#   gs_read(ws = "2017")
-
-
