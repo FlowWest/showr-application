e6908276ee939bfad54b6a9d5ad00df570fc1ac8
emanuel <erodriguez@flowwest.com>
2018-09-26 13:53:12 -0700

    move exploratory work from atu.R file to a new scratch file

---
+++ _scratch/hatching-distribution.R
@@ -0,0 +1,80 @@
+### NO TEMPERATURE DATA BEFORE 2010!!!!
+rd <- redd_data %>%
+  filter(counts > 0, year(date) >= 2010) %>% 
+  mutate(redd_id = row_number(date)) %>% 
+  rowwise() %>% 
+  do(
+    tibble(
+      date = seq(.$date, estimate_emergence(.$date, .$location) -1, by="day"), # this give a sequence from the seed day to the estiamted emergence value
+      seed_day = .$date,
+      location = .$location, 
+      counts = .$counts, # how many redds will exist in the water for this time
+      redd_id = .$redd_id
+    )
+  ) %>% ungroup() %>% 
+  mutate(cdec_gage = redd_cdec_lookup[location], 
+         location = factor(location, levels = unique(redd_data$location))) %>%
+  left_join(daily_temps)
+
+
+# the redd hatching distribution 
+chinook_hatch <- function(temp) {
+  log(0.08646) + (1.23473 * log(t + 2.26721)) 
+}
+
+rd %>% 
+  filter(year(date) == 2018) %>% 
+  group_by(redd_id) %>% 
+  mutate(hatching_devel = redd_hatching(daily_mean), 
+         hatching_accum = cumsum(hatching_devel)) %>%
+  transmute(
+    date,
+    seed_day, 
+    location, 
+    counts, 
+    exp(hatching_devel),
+    hatching_accum) %>% ungroup() %>%  
+  plot_ly(x=~date, y=~counts, type='bar', color=~location) %>% 
+  layout(barmode='stack') 
+
+rd_with_hatching <- rd %>% 
+  filter(year(date) == 2018) %>% 
+  group_by(redd_id) %>% 
+  mutate(hatching_devel = redd_hatching(daily_mean), 
+         hatching_accum = cumsum(hatching_devel)) %>%
+  transmute(
+    date,
+    seed_day, 
+    location, 
+    counts, 
+    exp(hatching_devel),
+    hatching_accum, 
+    hatching_day = ifelse(abs(hatching_accum-100) < 5, TRUE, FALSE)) %>% 
+  ungroup() %>% 
+  group_by(date) %>% 
+  summarise(
+    active_hatching = sum(hatching_day)
+  )
+
+
+# redd counts UP TO hatching stage
+rd_up_to_hatching <- rd_with_hatching %>% 
+  group_by(redd_id) %>% 
+  filter(hatching_accum <= 100) %>% ungroup() 
+
+plot_ly(data=rd_up_to_hatching,
+        x=~date, y=~counts, color=~location, type='bar') %>% 
+  add_lines(data=filter(rd, year(date) == 2018) %>% group_by(date) %>% summarise(counts=sum(counts)),
+            x=~date, y=~counts, inherit = FALSE) %>% 
+  layout(barmode='stack')
+
+plot_ly(data=filter(rd, year(date) == 2018),
+        x=~date, y=~counts, color=~location, type='bar') %>% 
+  add_lines(data=rd_up_to_hatching %>% group_by(date) %>% summarise(counts=sum(counts)),
+            x=~date, y=~counts, inherit = FALSE) %>% 
+  layout(barmode='stack')
+
+
+
+
+
--- atu.R
+++ atu.R
@@ -64,68 +64,6 @@ estimate_emergence <- function(spawn_date, redd_location) {
   } 
 }
 
-### NO TEMPERATURE DATA BEFORE 2010!!!!
-rd <- redd_data %>%
-  filter(counts > 0, year(date) >= 2010) %>% 
-  mutate(redd_id = row_number(date)) %>% 
-  rowwise() %>% 
-  do(
-    tibble(
-      date = seq(.$date, estimate_emergence(.$date, .$location) -1, by="day"), # this give a sequence from the seed day to the estiamted emergence value
-      seed_day = .$date,
-      location = .$location, 
-      counts = .$counts, # how many redds will exist in the water for this time
-      redd_id = .$redd_id
-    )
-  ) %>% ungroup() %>% 
-  mutate(cdec_gage = redd_cdec_lookup[location], 
-         location = factor(location, levels = unique(redd_data$location))) %>%
-  left_join(daily_temps)
-
-
-# the redd hatching distribution 
-chinook_hatch <- function(temp) {
-  log(0.08646) + (1.23473 * log(t + 2.26721)) 
-}
-
-rd %>% 
-  filter(year(date) == 2018) %>% 
-  group_by(redd_id) %>% 
-  mutate(hatching_devel = redd_hatching(daily_mean), 
-         hatching_accum = cumsum(hatching_devel)) %>%
-  transmute(
-    date,
-    seed_day, 
-    location, 
-    counts, 
-    exp(hatching_devel),
-    hatching_accum) %>% ungroup() %>%  
-  plot_ly(x=~date, y=~counts, type='bar', color=~location) %>% 
-  layout(barmode='stack') 
-
-rd %>% 
-  filter(year(date) == 2018) %>% 
-  group_by(redd_id) %>% 
-  mutate(hatching_devel = redd_hatching(daily_mean), 
-         hatching_accum = cumsum(hatching_devel)) %>%
-  transmute(
-    date,
-    seed_day, 
-    location, 
-    counts, 
-    exp(hatching_devel),
-    hatching_accum) %>% ungroup() %>%  
-  plot_ly(x=~date, y=~counts, type='bar', color=~location) %>% 
-  layout(barmode='stack') 
-  
-
-
-
-
-
-
-
-
 
 
 
